8731724b59d3d697b44382c9728ec9fe7373e71e
Umang Utkarsh <umang.ishan@gmail.com>
2023-07-28 14:17:05 +0530

    Added the main server file

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const userRoute = require('./routes/api/users');
+const authRoute = require('./routes/api/auth');
+const profileRoute = require('./routes/api/profile');
+const postsRoute = require('./routes/api/posts');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log('API running');
-});
+// Connect Database
+connectDB();
+
+// Init middleware
+// { extended: false }
+app.use(express.json());
+
+
+// Define Routes
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/posts', postsRoute);
+
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
