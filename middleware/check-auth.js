78bbbbe067a05e324336b9199d2fccc9d437bef5
Umang Utkarsh <umang.ishan@gmail.com>
2023-07-28 14:20:12 +0530

    Added middlewares

---
+++ middleware/check-auth.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+
+module.exports = (req, res, next) => {
+   // Get token from header
+   const token = req.header('x-auth-token');
+
+   // Check if no token
+   if (!token) {
+      return res.status(401).json({ mesg: "No token, authorization failed" });
+   }
+
+   // Verify token
+   try {
+      jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+         if (error) {
+            return res.status(401).json({ msg: "Token is not valid" });
+         }
+
+         else {
+            req.user = decoded.user;
+            next();
+         }
+      });
+   } catch (err) {
+      console.error('Something wrong with auth middleware');
+      res.status(500).json({ msg: "Server Error" });
+   }
+
+};
+
+
+// const decodedToken = jwt.verify(token, config.get('jwtSecret'));
+
+//       req.user = decodedToken.user;
+//       next();
