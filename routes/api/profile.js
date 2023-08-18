b7ce2a6dff1d068a91d6fdeee2d3009dc49d03cd
Umang Utkarsh <umang.ishan@gmail.com>
2023-08-18 20:55:15 +0530

    Get Github Repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // const axios = require('axios');
-// const config = require('config');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/check-auth');
 const { check, validationResult } = require('express-validator');
@@ -331,31 +332,31 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 
 
 
-// router.get('/github/:username', async (req, res) => {
+router.get('/github/:username', async (req, res) => {
   
-//   try {
-//     const options = {
-//       uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubCliendId')}&client_secret=${config.get('githubSecret')}`,
-//       method: 'GET',
-//       headers: { 'user-agent': 'node.js' }
-//     };
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
 
-//     request(options, (error, response, body) => {
-//       if (error) {
-//         console.error(error);
-//       }
+    request(options, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      }
 
-//       if (response.statusCode !== 200) {
-//         return res.status(404).json({ msg: "No GitHub profile found." });
-//       }
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No GitHub profile found." });
+      }
 
-//       res.json(JSON.parse(body));
-//     });
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 
 module.exports = router;
