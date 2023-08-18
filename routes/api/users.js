e846de45676021264de95bd7671ed0b1e6d898a9
Umang Utkarsh <umang.ishan@gmail.com>
2023-08-18 16:11:46 +0530

    User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 // const config = require('config');
-// const gravatar = require('gravatar');
-// const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 // const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 // const normalize = import('normalize-url');
@@ -13,79 +13,90 @@ const User = require('../../models/User');
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-// router.post('/', [
-//   check('name', 'Name is required').notEmpty(),
-//   check('email', 'Please provide a valid email').isEmail(),
-//   check('password', 'Please provide a valid password (min 6 characters)').isLength({ min: 6 })
-// ], async (req, res) => {
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please provide a valid email').isEmail(),
+  check('password', 'Please provide a valid password (min 6 characters)').isLength({ min: 6 })
+], async (req, res) => {
 
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ errors: errors.array() });
-//   }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  console.log(req.body);
 
-//   const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-//   try {
-//     let user = await User.findOne({ email });
+  try {
+    let user = await User.findOne({ email });
 
-//     if (user) {
-//       return res.status(400).json({ errors: [{ msg: "User already exists, please login instead" }] });
-//     }
+    if (user) {
+      res.status(400).json({ errors: [{ msg: "User already exists, please login instead" }] });
+    }
 
-//     const avatar = normalize(gravatar.url(email, {
-//       s: '200',
-//       r: 'pg',
-//       d: 'mm'
-//     }), { forceHttps: true });
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    });
 
-//     user = new User({
-//       name,
-//       email,
-//       password,
-//       avatar
-//     });
+    user = new User({
+      name,
+      email,
+      password,
+      avatar
+    });
     
-//     const salting = await bcrypt.genSalt(12);
-//     user.password = await bcrypt.hash(password, salting);
-//     await user.save();
+    const salting = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salting);
+    await user.save();
     
-//     const payload = {
-//       user: { id: user.id }
-//     };
+    // const payload = {
+    //   user: { id: user.id }
+    // };
 
-//     jwt.sign(payload, config.get('jwtSecret'), { expiresIn: '5 days' }, (err, token) => {
-//         if (err) {
-//           throw err;
-//         }
-//         res.json({ token });
-//       }
-//     );
+    // jwt.sign(payload, config.get('jwtSecret'), { expiresIn: '5 days' }, (err, token) => {
+    //     if (err) {
+    //       throw err;
+    //     }
+    //     res.json({ token });
+    //   }
+    // );
+
+    res.send('User registered');
     
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server error');
-//   }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 
-// });
+});
 
 
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-router.post('/', [
-  check('name', 'Name is required').not().isEmpty(),
-  check('email', 'Please include a valid email').isEmail(),
-  check('password', 'Please include a password of 6 or more characters').isLength({ min: 6 })
-], (req, res) => {
+// router.post('/', [
+//   check('name', 'Name is required').not().isEmpty(),
+//   check('email', 'Please include a valid email').isEmail(),
+//   check('password', 'Please include a password of 6 or more characters').isLength({ min: 6 })
+// ], async (req, res) => {
   
-  const errors = validationResult(req);
+//   const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  console.log(req.body);
-  res.send('User route');
-});
+//   if (!errors.isEmpty()) {
+//     return res.status(400).json({ errors: errors.array() });
+//   }
+//   console.log(req.body);
+
+//   try {
+//     let user = await User.
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server Error');
+//   }
+
+//   res.send('User route');
+// });
 
 module.exports = router;
